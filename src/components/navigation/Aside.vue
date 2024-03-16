d630e9851371c9877b9dbb8293c0711117a131c3
Glorfindel31 <cedric31flo@hotmail.com>
2024-03-16 07:00:51 +0700

    small morning changes

--- src/components/navigation/Aside.vue
+++ src/components/navigation/Aside.vue
@@ -4,12 +4,12 @@ import menuItems from './index';
 </script>
 
 <template>
-    <aside class="min-w-[30vw] p-4">
-        <ul class="flex flex-col gap-4 items-center">
+    <aside class="min-w-[300px] p-4 bg-slate-700">
+        <ul class="flex flex-col gap-4 items-left">
             <li>
                 <router-link
                     to="/"
-                    class="scroll-m-20 text-4xl tracking-tight lg:text-5xl"
+                    class="scroll-m-20 text-3xl tracking-tight lg:text-4xl"
                     >Cedric <span class="font-extrabold">Florentin</span>
                 </router-link>
             </li>
@@ -18,12 +18,18 @@ import menuItems from './index';
                     <component :is="item.component" />
                 </template>
                 <template v-else-if="item.type === 'link'">
-                    <Button variant="ghost" asChild>
+                    <Button
+                        variant="ghost"
+                        class="w-full items-start justify-start"
+                        asChild>
                         <router-link :to="item.to || '/'">{{ item.text }}</router-link>
                     </Button>
                 </template>
                 <template v-else-if="item.type === 'external'">
-                    <Button variant="ghost" asChild>
+                    <Button
+                        variant="ghost"
+                        class="w-full items-start justify-start"
+                        asChild>
                         <a :href="item.href" target="_blank">{{ item.text }}</a>
                     </Button>
                 </template>
